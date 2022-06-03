8c5fcf3fcc4deb52fbf8bcd3608064d9aa1b9d18
Leah <amelia_rawls@ccpsnet.net>
2022-06-02 21:03:06 -0400

    working on server issues

--- server.js
+++ server.js
@@ -27,8 +27,9 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
