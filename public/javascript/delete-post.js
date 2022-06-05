7b0c358c10d948c49fe60ea9def59f99f041013e
Koehler <ckoehler16@gmail.com>
2022-06-05 16:36:29 -0400

    javascript tweak

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -5,7 +5,13 @@ async function deleteFormHandler(event) {
         window.location.toString().split('/').length - 1
     ];
     const response = await fetch(`/api/posts/${id}`, {
-        method: 'DELETE'
+        method: 'DELETE',
+        body: JSON.stringify({
+            post_id: id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     });
 
     if (response.ok) {
