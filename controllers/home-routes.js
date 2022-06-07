83106033fd4857e3487bcdac44883068a6f0c1cd
Chris Porter <cap8095@gmail.com>
2022-06-07 12:38:26 -0400

    making landing page and incorporating the animejs animation

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,69 +1,35 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
 
-router.get('/', (req, res) => {
-  Post.findAll({
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at'],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-
-      res.render('homepage', { posts, loggedIn: req.session.loggedIn });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+router.get("/", (req, res) => {
+  res.render("landingpage");
 });
 
-router.get('/post/:id', (req, res) => {
+router.get("/post/:id", (req, res) => {
   Post.findOne({
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-    ],
+    attributes: ["id", "post_url", "title", "created_at"],
     include: [
       {
         model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
         include: {
           model: User,
-          attributes: ['username']
-        }
+          attributes: ["username"],
+        },
       },
       {
         model: User,
-        attributes: ['username']
-      }
-    ]
+        attributes: ["username"],
+      },
+    ],
   })
-    .then(dbPostData => {
+    .then((dbPostData) => {
       if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
+        res.status(404).json({ message: "No post found with this id" });
         return;
       }
 
@@ -71,22 +37,21 @@ router.get('/post/:id', (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render('single-post', { post, loggedIn: req.session.loggedIn });
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
-
 
 module.exports = router;
---
+++ public/javascript/index.js
@@ -0,0 +1,65 @@
+//import animejs
+// const anime = require("animejs");
+
+// Wrap every letter in a span
+
+var options = {
+  opacityIn: [0, 1],
+  scaleIn: [0.2, 1],
+  scaleOut: 3,
+  durationIn: 800,
+  durationOut: 600,
+  delay: 500,
+  easing: "easeInExpo",
+};
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".text-animation .one",
+    opacity: options.opacityIn,
+    scale: options.scaleIn,
+    duration: options.durationIn,
+  })
+  .add({
+    targets: ".text-animation .one",
+    opacity: 0,
+    scale: options.scaleOut,
+    easing: options.easing,
+    duration: options.durationOut,
+    delay: options.delay,
+  })
+  .add({
+    targets: ".text-animation .two",
+    opacity: options.opacityIn,
+    scale: options.scaleIn,
+    duration: options.durationIn,
+  })
+  .add({
+    targets: ".text-animation .two",
+    opacity: 0,
+    scale: options.scaleOut,
+    easing: options.easing,
+    duration: options.durationOut,
+    delay: options.delay,
+  })
+  .add({
+    targets: ".text-animation .three",
+    opacity: options.opacityIn,
+    scale: options.scaleIn,
+    duration: options.durationIn,
+  })
+  .add({
+    targets: ".text-animation .three",
+    opactiy: 0,
+    scale: options.scaleOut,
+    easing: options.easing,
+    duration: options.durationOut,
+    delay: options.delay,
+  })
+  .add({
+    targets: ".text-animation",
+    opacity: 0,
+    duration: 500,
+    delay: 500,
+  });
