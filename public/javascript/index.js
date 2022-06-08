d892ee4d71a8ee1c33c1d82b301371f9925e2b39
Chris Porter <cap8095@gmail.com>
2022-06-08 12:15:13 -0400

    adding the paragraph and trying to get the word pantry to go away

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -10,9 +10,17 @@ var options = {
   durationIn: 800,
   durationOut: 600,
   delay: 500,
-  easing: "easeInExpo",
+  easing: "easeInOutExpo",
 };
-
+var paragraph = {
+  opacityIn: [0, 1],
+  scaleIn: [0.2, 1],
+  scaleOut: 3,
+  durationIn: 100,
+  durationOut: 100,
+  delay: 100,
+  easing: "easeInOutExpo",
+};
 anime
   .timeline({ loop: false })
   .add({
@@ -45,21 +53,39 @@ anime
   })
   .add({
     targets: ".text-animation .three",
-    opacity: options.opacityIn,
-    scale: options.scaleIn,
-    duration: options.durationIn,
+    opacity: paragraph.opacityIn,
+    scale: paragraph.scaleIn,
+    duration: paragraph.durationIn,
   })
   .add({
     targets: ".text-animation .three",
     opactiy: 0,
-    scale: options.scaleOut,
+    scale: 3,
+    easing: paragraph.easing,
+    duration: paragraph.durationOut,
+    delay: options.delay,
+  })
+
+  .add({
+    targets: ".text-animation .four",
+    opacity: options.opacityIn,
+    scale: options.scaleIn,
+    duration: options.durationIn,
+  })
+
+  .add({
+    targets: ".text-animation .four",
+    opactiy: 0,
+    // scale: options.scaleOut,
     easing: options.easing,
     duration: options.durationOut,
     delay: options.delay,
   })
-  .add({
-    targets: ".text-animation",
-    opacity: 0,
-    duration: 500,
-    delay: 500,
-  });
+
+  .timeline({ loop: true });
+// .add({
+//   targets: ".text-animation",
+//   opacity: 0,
+//   duration: 500,
+//   delay: 500,
+// });
