47fca6528002ef94b429b88ad53fd247d616cb1c
Koehler <ckoehler16@gmail.com>
2022-06-11 22:16:16 -0400

    fixed header, delete rte, css, and more

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -79,29 +79,29 @@ router.post('/', withAuth, (req, res) => {
 });
 
 // update a recipe link
-// router.put('/:id', withAuth, (req, res) => {
-//    Post.update(
-//       {
-//          title: req.body.title
-//       },
-//       {
-//          where: {
-//             id: req.params.id
-//          }
-//       }
-//    )
-//    .then(dbPostData => {
-//       if (!dbPostData) {
-//          res.status(404).json({ message: 'No post found with this id' });
-//          return;
-//       }
-//       res.json(dbPostData);
-//    })
-//    .catch(err => {
-//       console.log(err);
-//       res.status(500).json(err);
-//    });
-// });
+router.put('/:id', withAuth, (req, res) => {
+   Post.update(
+      {
+         title: req.body.title
+      },
+      {
+         where: {
+            id: req.params.id
+         }
+      }
+   )
+   .then(dbPostData => {
+      if (!dbPostData) {
+         res.status(404).json({ message: 'No post found with this id' });
+         return;
+      }
+      res.json(dbPostData);
+   })
+   .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+   });
+});
 
 // delete a recipe link
 router.delete('/:id', withAuth, (req, res) => {
